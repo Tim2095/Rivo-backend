4e986f70a72f2e969ec049cc1f552a532b309cb4
Timur <timur.mosenkov@gmail.com>
2024-10-08 20:34:19 +0300

    create error handling in task controller

--- controllers/task.js
+++ controllers/task.js
@@ -1,20 +1,41 @@
-const taskRouter = require('express').Router()
-const User = require('../models/user')
-const jwt = require('jsonwebtoken')
+const taskRouter = require("express").Router();
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
 
-
-const getTokenFrom = request => {
-  const authorization = request.get('authorization')
-  if(authorization && authorization.startsWith('Bearer ')) {
-    return authorization.replace('Bearer ', '')
+const getTokenFrom = (request) => {
+  const authorization = request.get("authorization");
+  if (authorization && authorization.startsWith("Bearer ")) {
+    return authorization.replace("Bearer ", "");
   }
-  return null
-} 
+  return null;
+};
 
+taskRouter.post("/", async (req, res, next) => {
+  try {
+    const title = req.body.taskTitle;
+    const description = req.body.description;
 
-taskRouter.post('/', async (req, res) => {
-  const token = getTokenFrom(req)
-  console.log(token)
-})
+    if (!description || !title) {
+      const error = new Error("Task and Title fields are reqired");
+      error.statusCode = 400;
+      return next(error);
+    }
+    const token = getTokenFrom(req);
+    if (!token) {
+      const error = new Error('Missing or invalid token')
+      error.statusCode = 401 
+      return next(error)
+    }
 
-module.exports = taskRouter
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if(!decodedToken.id) {
+      const error = new Error('Invalid token')
+      error.statusCode = 401 
+      return next(error)
+    }
+  } catch (err) {
+    next(err)
+  }
+});
+
+module.exports = taskRouter;
