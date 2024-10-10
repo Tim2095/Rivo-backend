771f3b04bac962920b21ae329f159342ad03ee7a
Timur <timur.mosenkov@gmail.com>
2024-10-10 21:36:30 +0300

    add created day of task to schema of db

--- models/user.js
+++ models/user.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const taskSchema = mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String },
+  dateCreated: {type: String},
   completed: { type: Boolean, required: true }
 });
 
