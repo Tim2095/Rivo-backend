765f7e8dca2b5f9fefb80052fd95836cc808d679
Timur <timur.mosenkov@gmail.com>
2024-10-17 11:57:08 +0300

    remove _id field of tasks and replace it just with id

--- models/user.js
+++ models/user.js
@@ -7,6 +7,14 @@ const taskSchema = mongoose.Schema({
   completed: { type: Boolean, required: true }
 });
 
+taskSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
 
 const userSchema = mongoose.Schema({
   firstname: String,
