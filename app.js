65537a33afc198bafea5af66b328913818cf80b5
Timur <timur.mosenkov@gmail.com>
2024-11-07 23:44:42 +0200

    fix error

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use("/api/user", userController);
 app.use("/api/login", loginController);
 app.use("/api/new-task", taskController);
 
-app.use(express.static('dist'))
+app.use(express.static(path.join(__dirname, "dist")));
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
