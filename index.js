3bc7d9a1115e724462ecc3c2aa23f2f709b1d7fc
Timur <timur.mosenkov@gmail.com>
2024-11-08 10:33:46 +0200

    modify index

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require("./app");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3003;
 
 app.listen(PORT, () => {
   console.log(`the app is running at port ${PORT}`);
